bfab21397dcf21cda2ad2f8fc0ed854a35887ca9
caravagn <gcaravagn@gmail.com>
2018-12-13 23:17:32 +0000

    Improved plot

--- R/mobster_dataset_plot.R
+++ R/mobster_dataset_plot.R
@@ -24,107 +24,122 @@ plot.mbst_data = function(x,
   
   if(!by_genotype)
   { 
-    # Full VAF plot
+    # Full VAF plot, and zoom
     vaf = VAF(x) %>% filter(value > 0)
+    lf_vaf = vaf %>% filter(value < 0.2)
+    
+    # Plus coverage
+    DP_val = DP(x) %>% filter(value > 0)
+    
+    stats_DP_val = quantile(DP_val %>% pull(value))
+    stats_DP_val = data.frame(value = stats_DP_val, quantile = names(stats_DP_val))
+    # 
     
     max.VAF = max(vaf$value)
     if(max.VAF < 1) max.VAF = 1
     
     pl_vaf = ggplot(vaf, aes(value, fill = sample)) +
-      geom_histogram(binwidth = 0.01) +
-      # geom_density() +
-      facet_wrap(~sample, nrow = 1, scales = scales) +
+      facet_wrap(~sample, nrow = 1, scales = 'free') +
       theme_light(base_size = 8) +
       guides(fill = FALSE) +
       theme(legend.position="bottom",
             legend.text = element_text(size = 8),
             legend.key.size = unit(.3, "cm")
       ) +
-      geom_vline(aes(xintercept = 0.5), colour = 'red', linetype = "longdash", size = .3) +
-      geom_vline(aes(xintercept = 0.25), colour = 'red', linetype = "longdash", size = .3) +
-      geom_vline(aes(xintercept = 0.33), colour = 'blue', linetype = "longdash", size = .3) +
+      scale_fill_brewer(palette = 'Set1') +
+      geom_vline(aes(xintercept = 0.5), colour = 'black', size = .3) +
+      geom_vline(aes(xintercept = 0.25), colour = 'darkblue', linetype = "longdash", size = .5) +
+      geom_vline(aes(xintercept = 0.33), colour = 'steelblue', linetype = "longdash", size = .5) +
       geom_vline(aes(xintercept = 0.66), colour = 'blue', linetype = "longdash", size = .3) +
       geom_vline(aes(xintercept = 1), colour = 'black', linetype = "longdash", size = .2) +
+      geom_histogram(binwidth = 0.01, alpha = .8) +
       # geom_density() +
-      xlim(0, max.VAF) + 
+      xlim(0, max.VAF) +
       labs(
-        title = "Variant Allele Frequency (VAF)",
-        x = 'VAF')
+        title = "Adjusted VAF",
+        x = 'Data', 
+        y = 'Counts'
+      )
     
     # Low-frequency VAF
     vaf = vaf %>% filter(value < 0.2)
     
     pl_vaf_lfreq = ggplot(vaf, aes(value, fill = sample)) +
-      geom_histogram(binwidth = 0.01) +
       # geom_density() +
-      facet_wrap(~sample, nrow = 1, scales = scales) +
+      facet_wrap(~sample, nrow = 1, scales = 'free') +
       theme_light(base_size = 8) +
+      scale_fill_brewer(palette = 'Set1') +
       guides(fill = FALSE) +
       theme(legend.position="bottom",
             legend.text = element_text(size = 8),
             legend.key.size = unit(.3, "cm")
       ) +
       labs(
-        title = "Low-freq. VAF < 0.20",
-        x = 'VAF')
+        title = "Adjusted low-frequency VAF (<20%)",
+        x = 'VAF', 
+        y = 'Counts'
+      ) +
+      geom_vline(aes(xintercept = 0.01), colour = 'gray', size = .2) +
+      geom_vline(aes(xintercept = 0.02), colour = 'gray', size = .2) +
+      geom_vline(aes(xintercept = 0.03), colour = 'gray', size = .2) +
+      geom_vline(aes(xintercept = 0.04), colour = 'gray', size = .2) +
+      geom_vline(aes(xintercept = 0.05), colour = 'gray', size = .2) +
+      geom_histogram(binwidth = 0.001, alpha = .8)
     
-    DP_val = DP(x) %>% filter(value > 0)
     
-    stats_DP_val = quantile(DP_val %>% pull(value))
-    stats_DP_val = data.frame(value = stats_DP_val, quantile = names(stats_DP_val))
     
     pl_DP = ggplot(DP_val, aes(value, fill = sample)) +
-      geom_histogram(binwidth = 1) +
+      geom_histogram(bins = 100, alpha = .8) +
       geom_vline(xintercept = stats_DP_val$value, size = .3, linetype = 'dashed', show.legend = TRUE) +
       geom_vline(xintercept = median(DP_val$value), size = .5, colour = 'steelblue') +
-      facet_wrap(~sample, nrow = 1,  scales = scales) +
-      guides(fill = FALSE, color = guide_legend(title="Quantile")) +
+      facet_wrap(~sample, nrow = 1,  scales = 'free') +
+      guides(fill = FALSE, color = FALSE) +
+      scale_fill_brewer(palette = 'Set1') +
       labs(
-        title = "DP histogram (raw data)",
+        title = "Coverage distribution",
         subtitle = paste0('Quantiles (dashed):', 
                           paste0(stats_DP_val$quantile, collapse = ', '),
                           '. Median (blue)  = ', round(median(DP_val$value), 0), 
                           'x (var. ', round(var(DP_val$value), 0), ')'),
+        y = 'Counts',
         x = 'Coverage (DP)') +
       theme_light(base_size = 8) +
       theme(legend.position="bottom",
             legend.text = element_text(size = 8),
             legend.key.size = unit(.3, "cm")
-      ) 
+      ) +
+      scale_x_log10()
     
-    # 
-    # v = DP_val %>%
-    #   group_by(sample) %>%
-    #   summarize(
-    #     # n = n(),
-    #     min = min(value),
-    #     max = max(value),
-    #     mean = mean(value),
-    #     sd = sd(value),
-    #     median = median(value)
-    #   ) 
-    # 
-    # # v.size = v %>% select(-sample)
-    # # C.v.size = apply(v.size, 2, max)
-    # # v.size = data.frame(t(t(v.size)/C.v.size))
-    # # v.size$sample = v$sample
-    # # 
-    # v = reshape2::melt(v)
-    # v$value = round(v$value)
-    # 
-    # ggballoonplot(v, 
-    #               x = "sample", y = "variable",
-    #               size = "value", fill = "value", size.range = c(1, 5), ) 
-    # scale_fill_gradientn(colors = my_cols) +
-    # guides(size = FALSE) +
-    # facet_wrap(~variable, scales = 'free')
+    
+    pl_cov_vaf = ggplot(
+      bind_rows(DP_val, vaf) %>% spread(variable, value), 
+      aes(x = DP, y = VAF, color = sample)) +
+      geom_point(alpha = .1) +
+      geom_vline(xintercept = stats_DP_val$value, size = .3, linetype = 'dashed', show.legend = TRUE) +
+      geom_vline(xintercept = median(DP_val$value), size = .5, colour = 'steelblue') +
+      facet_wrap(~sample, nrow = 1,  scales = 'free') +
+      guides(fill = FALSE, color = FALSE) +
+      scale_color_brewer(palette = 'Set1') +
+      labs(title = "Coverage versus VAF",
+           subtitle = paste0('Quantiles (dashed):', 
+                             paste0(stats_DP_val$quantile, collapse = ', '),
+                             '. Median (blue)  = ', round(median(DP_val$value), 0), 
+                             'x (var. ', round(var(DP_val$value), 0), ')'),
+           x = 'Coverage (DP)') +
+      theme_light(base_size = 8) +
+      theme(legend.position="bottom",
+            legend.text = element_text(size = 8),
+            legend.key.size = unit(.3, "cm")) +
+      scale_x_log10()
     
     
     figure = ggpubr::ggarrange(
-      ggpubr::ggarrange(pl_vaf, pl_vaf_lfreq, ncol = 1, heights = c(2, 1.5), nrow = 2),
+      pl_vaf, 
+      pl_vaf_lfreq, 
+      pl_cov_vaf,
       pl_DP,
       ncol = 1,
-      nrow = 2)
+      nrow = 4)
     
     figure = ggpubr::annotate_figure(figure, top = title)
     
