9ea934eb207800f4f5549c4e80f3862ce3b21d16
caravagn <gcaravagn@gmail.com>
2019-11-14 16:29:41 +0000

    Fixing dnds annotation crash in weird cases

--- R/annotate_genes_from_locations.R
+++ R/annotate_genes_from_locations.R
@@ -1,10 +1,19 @@
-annotate_genes_from_locations = function(x)
+  annotate_genes_from_locations = function(x)
 {
   library(Homo.sapiens)
   
   if(!all(c('chr', 'from', 'to', 'ref', 'alt') %in% colnames(x$data)))
     stop("Missing genomic coordinates (chr, from, ref, alt) from the input data, cannot annotate genes.")
   
+    if(any(!grepl('chr', x$data$chr)))
+    {
+      message("Adding `chr` to the chromosome names (1 -> chr1)")
+      x$data = x$data %>%
+        dplyr::mutate(
+          chr = ifelse(grepl('chr', chr), chrom, paste0('chr', chr))
+        )
+    }
+    
   mutations = mobster::Clusters(x) %>%
     dplyr::select(chr, from, ref, alt) %>%
     dplyr::rename(chrom = chr, start = from) %>% 
@@ -18,15 +27,15 @@ annotate_genes_from_locations = function(x)
     data.frame(stringsAsFactors = FALSE) %>%
     makeGRangesFromDataFrame 
   
-  GR_df_overlaps = subsetByOverlaps(genes(TxDb.Hsapiens.UCSC.hg19.knownGene), GR_df_mutations) %>%
+  GR_df_overlaps = subsetByOverlaps(GenomicFeatures::genes(TxDb.Hsapiens.UCSC.hg19.knownGene), GR_df_mutations) %>%
     data.frame(stringsAsFactors = FALSE) %>% 
-    as_tibble() %>%
-    mutate(
+    tibble::as_tibble() %>%
+    dplyr::mutate(
       seqnames = paste(seqnames),
       strand = paste(strand)
     ) %>%
-    left_join(as.data.frame(org.Hs.egSYMBOL), by = 'gene_id') %>%
-    select(-strand, -gene_id, -width)
+    dplyr::left_join(as.data.frame(org.Hs.egSYMBOL), by = 'gene_id') %>%
+    dplyr::select(-strand, -gene_id, -width)
   
   colnames(GR_df_overlaps) = c('chr', 'from', 'to', 'gene')
   colnames(mutations) = c('chr', 'from', 'to')
@@ -34,18 +43,18 @@ annotate_genes_from_locations = function(x)
   final_mapping = lapply(1:nrow(GR_df_overlaps),
                          function(x){
                            mutations %>%
-                             filter(
+                             dplyr::filter(
                                chr == GR_df_overlaps$chr[x],
                                from >= GR_df_overlaps$from[x],
                                to <= GR_df_overlaps$to[x]
                              ) %>%
-                             mutate(gene = GR_df_overlaps$gene[x])
+                             dplyr::mutate(gene = GR_df_overlaps$gene[x])
                          }) %>%
     Reduce(f = bind_rows)
   
   mutations_enriched = mutations %>%
-    left_join(final_mapping, by = c('chr', 'from', 'to')) %>%
-    distinct(chr, from, to, .keep_all = T)
+    dplyr::left_join(final_mapping, by = c('chr', 'from', 'to')) %>%
+    dplyr::distinct(chr, from, to, .keep_all = T)
   
   # table(mutations_enriched$gene) %>% sort(decreasing = TRUE) %>% pioDisp()
   mobster::Clusters(x)  %>%
