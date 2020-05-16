632af85a3432ef6be30cb113f50d220f3333dbfe
Giulio Caravagna <gcaravagn@gmail.com>
2020-05-16 10:31:16 +0200

    Plot colours bug fixing

--- R/S3_methods_plot.R
+++ R/S3_methods_plot.R
@@ -1,90 +1,137 @@
 #' Plot a MOBSTER fit.
 #'
 #' @param x An object of class \code{"dbpmm"}.
-#' @param alpha Alpha value for the colors of the histogram
-#' @param colors If provided, these colours will be used for each cluster.
-#' If a subset of colours is provided, palette Set1 from \code{RColorBrewer} is used.
-#' By default the tail colour is provided as 'gainsboro'.
+#' @param beta_colors A vector of colors that are used to colour the Beta clusters.
+#' Colors are used by order for \code{"C1"}, \code{"C2"}, \code{"C3"}, etc. If too
+#' few colours are used the \code{rainbow} palette is used instead of \code{beta_colors}.
+#' In all cases the colour of the tail is set by the other parameter \code{tail_color}.
+#' @param tail_color The colour of the tail cluster, if any.
 #' @param cutoff_assignment Parameters passed to run function \code{Clusters} which
 #' returns the hard clustering assignments for the histogram plot if one wants to plot
 #' only mutations with responsibility above this parameter.
 #' @param annotation_extras A dataframe that contains a label column, and a VAF value.
 #' The labels will be annotated to the corresponding clusters of the VAF values.
-#' @param secondary_axis \code{NULL} to leave the second axis empty, \code{"SSE"} to 
+#' @param secondary_axis \code{NULL} to leave the second axis empty, \code{"SSE"} to
 #' report the cumulative percentage of SSE error, and \code{"N"} to report the
 #' cumulative number of mutations.
-#' @param ... 
+#' @param ...
 #'
 #' @return A ggplot object for the plot.
 #'
 #' @import sads
 #' @import ggplot2
-#' 
+#'
 #' @exportS3Method plot dbpmm
 #' @export
 #'
-#' @examples 
+#' @examples
 #' data(fit_example)
 #' plot(fit_example$best)
 #' plot(fit_example$best, secondary_axis = 'SSE')
-#' plot(fit_example$best, secondary_axis = 'N')
-#' plot(fit_example$best, alpha = 1, cutoff_assignment = .7)
-#' plot(fit_example$best, colors =  c(`Tail` = 'gainsboro', `C1` = 'darkorange'))
+#' plot(fit_example$best, cutoff_assignment = .7)
+#' plot(fit_example$best, beta_colors = c("indianred3", "orange"))
+#' plot(fit_example$best, tail_color = 'black')
 plot.dbpmm = function(x,
-                      alpha = .7,
-                      colors = c(`Tail` = 'gainsboro'),
                       cutoff_assignment = 0,
+                      beta_colors = RColorBrewer::brewer.pal(n = 9, 'Set1'),
+                      tail_color = "gainsboro",
                       annotation_extras = NULL,
                       secondary_axis = NULL,
-                      ...
-                      )
+                      ...)
 {
-  is_mobster_fit(x)
+  mobster:::is_mobster_fit(x)
   
+  # -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+  # Plotting variables
+  # -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
   binwidth = 0.01
-  histogram.main = ifelse(is.null(x$description), 'MOBSTER fit', x$description)
-  
-  # Prepare variables
   domain = seq(0, 1, binwidth)
-
+  
   labels = names(mobster:::.params_Pi(x))
   labels.betas = mobster:::.params_Beta(x)$cluster
-
+  
   pi = mobster:::.params_Pi(x)
-
-  # Main plotting data
-  plot_data = mobster:::Clusters(x, cutoff_assignment)
+  
+  # -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+  # Plotting data
+  # -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+  # Clustering assignments and clusters
+  plot_data = mobster:::Clusters(x, cutoff_assignment) %>% dplyr::arrange(cluster)
+  
   clusters = sort(unique(plot_data$cluster), na.last = TRUE)
   
-  # Text for the plot -- convergence
+  # Beta peaks
+  Beta_peaks = x$Clusters %>%
+    dplyr::filter(type == 'Mean', cluster != 'Tail')
+  
+  # Component mixture density and total as \sum_i pi_i f_i
+  densities = mobster:::template_density(x,
+                                         x.axis = domain,
+                                         binwidth = binwidth,
+                                         reduce = TRUE)
+  
+  total_densities = tibble::as_tibble(densities) %>%
+    group_by(x) %>%
+    summarise(y = sum(y), cluster = 'f(x)')
+  
+  # -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+  # Plotting aestetics
+  # -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+  
+  # Colours per cluster - tail is reserved
+  Beta_colors = pio:::nmfy(paste0('C', 1:length(beta_colors)), beta_colors)
+  MOBSTER_CLUSTER_COLORS = c(Beta_colors, `Tail` = tail_color)
+  
+  if (all(clusters %in% names(MOBSTER_CLUSTER_COLORS)))
+  {
+    MOBSTER_CLUSTER_COLORS = MOBSTER_CLUSTER_COLORS[names(MOBSTER_CLUSTER_COLORS) %in% clusters]
+  }
+  else
+  {
+    warning("You did not pass enough input colours, using rainbow palette")
+    MOBSTER_CLUSTER_COLORS = rainbow(length(clusters))
+  }
+  
+  # Plot title
+  plot_title = ifelse(all(is.null(x$description)), 'mobster fit', x$description)
+  
+  # Plot substitle
+  plot_subtitle = NULL
+  
+  pi = sort(pi)
+  pi = paste0(names(pi), ' ', round(pi * 100, 1), '%', collapse = ', ')
+  
+  plot_subtitle = paste0("n = ", x$N, "; ", pi)
+  
+  # Plot caption
+  plot_caption = NULL
+  
   conv.steps = length(x$all.NLL)
   conv.epsilon = 0
   if (conv.steps >= 2)
     conv.epsilon = abs(rev(x$all.NLL)[1] - rev(x$all.NLL)[2])
   conv.epsilon = formatC(conv.epsilon, format = "e", digits = 0)
-
-  sse = max(mobster:::.compute_fit_sqerr(x, binning = binwidth)$cum.y)
-  sse = formatC(sse, format = "e", digits = 0)
-
-  label.fit = bquote(
-    .(x$fit.type) *
-      " (" * omega * " = " * .(conv.steps) ~ '; ' * epsilon ~ '=' ~ .(conv.epsilon) *
-      "; SSE" ~ .(sse) * '), LV > ' * .(cutoff_assignment)
-  )
-
-  # -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
-  # Main ggplot object is the histogram 
-  # -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
   
-  hist_pl = ggplot(
-    plot_data, 
-    aes(VAF, 
-        fill = factor(cluster, levels = clusters), 
-        y = ..count.. /sum(..count..))) +
-    geom_histogram(alpha = alpha,
-                   color = NA,
-                   position = 'identity',
-                   binwidth = binwidth) +
+  plot_caption = bquote(
+    .(x$fit.type) *
+      " (" * .(conv.steps) ~ 'steps; ' * epsilon ~ '=' ~ .(conv.epsilon) *
+      '), ' * z["nk"] * ' > ' * .(cutoff_assignment)
+  )
+  
+  # -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+  # Main ggplot object is the histogram
+  # -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+  hist_pl = ggplot(plot_data,
+                   aes(VAF,
+                       fill = factor(cluster),
+                       y = ..count.. / sum(..count..))) +
+    geom_histogram(
+      alpha = .6,
+      color = NA,
+      position = 'identity',
+      binwidth = binwidth
+    ) +
+    scale_fill_manual(values = MOBSTER_CLUSTER_COLORS) +
     geom_vline(
       xintercept = min(x$data$VAF),
       colour = 'black',
@@ -92,10 +139,32 @@ plot.dbpmm = function(x,
       size = .3
     ) +
     guides(fill = guide_legend(title = "Cluster")) +
+    geom_line(data = densities,
+              aes(
+                y = y,
+                x = x,
+                color = factor(cluster)
+              ),
+              size = 1) +
+    geom_line(
+      data = total_densities %>% mutate(y = y + max(total_densities$y, na.rm = TRUE) * 0.02),
+      aes(y = y, x = x),
+      color = 'black',
+      alpha = .8,
+      size = .5,
+      linetype = 'dashed',
+      inherit.aes = FALSE
+    ) +
+    geom_vline(data = Beta_peaks,
+               aes(xintercept = fit.value,
+                   color = factor(cluster)),
+               linetype = "longdash") +
+    scale_color_manual(values = MOBSTER_CLUSTER_COLORS) +
+    guides(color = FALSE) +
     labs(
-      title = bquote(.(histogram.main)),
-      # subtitle = annotation,
-      caption = label.fit,
+      title = bquote(.(plot_title)),
+      subtitle = plot_subtitle,
+      caption = plot_caption,
       x = "Observed Frequency",
       y = "Density"
     ) +
@@ -103,78 +172,19 @@ plot.dbpmm = function(x,
     theme(
       panel.background = element_rect(fill = 'white'),
       plot.caption = element_text(color = ifelse(x$status, "darkgreen",  "red"))
-    ) + coord_cartesian(clip = 'off')
+    ) +
+    coord_cartesian(clip = 'off')
   
-  # Get the maximum of the histogram
-  hist_yMax = max(ggplot_build(hist_pl)$data[[1]]$y)
   
-  # -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
-  # We add the density to the histogram
-  # -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
-  # Template density values
-  densities = mobster:::template_density(x,
-                               x.axis = domain,
-                               binwidth = binwidth,
-                               reduce = TRUE)
-
-  # Add the trace and the mean of each component
-  hist_pl = hist_pl +
-    geom_line(data = densities,
-              aes(
-                y = y,
-                x = x,
-                color = factor(cluster, levels = clusters)
-              ),
-              size = 1) +
-    # scale_color_manual(values = colors, labels = names(colors)) +
-    guides(color = FALSE)
-  
-  # The new max, if the density is too high we set the max to the hist max
-  hist_den_yMax = max(ggplot_build(hist_pl)$data[[1]]$y)
-  
-  if(hist_yMax < hist_den_yMax ) hist_pl = hist_pl + ylim(0, hist_yMax)
-
   # -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
-  # Beta peaks (means) annotated to the plot
+  # Dashed SSE behind the overall density (optional), scaled to percentage
   # -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
-  Beta_peaks = x$Clusters %>%
-    dplyr::filter(type == 'Mean', cluster != 'Tail')
-
-  hist_pl = hist_pl +
-    geom_vline(data = Beta_peaks,
-               aes(xintercept = fit.value, 
-                   color = factor(cluster, levels = clusters)),
-               linetype = "longdash")
-
-  # -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
-  # Overall mixture density as \sum_i pi_i f_i
-  # -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
-  densities = tibble::as_tibble(densities)
-  densities = densities %>% group_by(x) %>% summarise(y = sum(y), cluster = 'f(x)')
-
-  m = max(densities$y, na.rm = TRUE)
-
-  hist_pl = hist_pl +
-    geom_line(
-      data = densities %>% mutate(y = y + m * 0.02),
-      aes(y = y, x = x),
-      color = 'black',
-      alpha = .8,
-      size = .5,
-      linetype = 'dashed',
-      inherit.aes = FALSE
-    )
-
-  # -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
-  # Dashed SSE behing the overall density
-  # -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
-  error = suppressWarnings(mobster:::.compute_fit_sqerr(x, binning = binwidth))
-
-  # scale to percentage for plotting
-  if(!is.null(secondary_axis))
+  if (!is.null(secondary_axis))
   {
-    if(secondary_axis == "SSE")
+    if (secondary_axis == "SSE")
     {
+      error = suppressWarnings(mobster:::.compute_fit_sqerr(x, binning = binwidth))
+      
       me = max(error$cum.y)
       error = error %>% dplyr::mutate(cum.y = (cum.y / me) * m)
       
@@ -188,17 +198,17 @@ plot.dbpmm = function(x,
           linetype = 'dashed',
           inherit.aes = FALSE
         ) +
-        scale_y_continuous(sec.axis = sec_axis( ~ . / m * 100, name = "SSE [cumulative %]"))
+        scale_y_continuous(sec.axis = sec_axis(~ . / m * 100, name = "SSE [cumulative %]"))
     }
     
     # if(secondary_axis == "N")
     # {
     #   xbins = hist(x$data$VAF, breaks = domain, plot = F)$counts
     #   xcumc = cumsum(xbins)
-    #   
+    #
     #   brk = seq(0, x$N, by = x$N/4)
-    # 
-    #   # hist_pl = 
+    #
+    #   # hist_pl =
     #     hist_pl +
     #     geom_line(
     #       data =  data.frame(x = domain[-1], y = (xcumc/x$N) * m),
@@ -210,8 +220,8 @@ plot.dbpmm = function(x,
     #       inherit.aes = FALSE
     #     ) +
     #     scale_y_continuous(
-    #       sec.axis = sec_axis( ~ . , 
-    #                            name = "Counts (n)", 
+    #       sec.axis = sec_axis( ~ . ,
+    #                            name = "Counts (n)",
     #                            breaks = brk / x$N * m,
     #                            labels = brk
     #                            )
@@ -220,27 +230,10 @@ plot.dbpmm = function(x,
     
   }
   
-
   # -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
-  # Annotate mixting proportions
+  # Extra annotations (param) plus drivers
   # -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
-  pi = sort(pi)
-  pi = paste0(names(pi), ' ', round(pi * 100, 1), '%', collapse = ', ')
+  hist_pl = mobster:::add_extra_plot_annotations(x, annotation_extras, base_plot = hist_pl)
   
-  hist_pl = hist_pl +
-    labs(
-      subtitle = paste0("N = ", x$N, "; ", pi)
-    )
-  
-  # -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
-  # Custom coloring
-  # -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
-  pl = mobster:::add_fill_color_pl(x, hist_pl, colors)
-  
-  # The plot allows to annotate some extra events etc.
-  pl = mobster:::add_extra_plot_annotations(x, annotation_extras, base_plot = pl)
-    
-
-  return(pl)
+  return(hist_pl)
 }
-
